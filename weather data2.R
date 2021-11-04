246b4ff0724f054605c2f7612ab4f53de6924ddb
lepromatous <tim.wiemken@gmail.com>
2021-11-04 11:17:00 -0500

    weather update

--- weather data2.R
+++ weather data2.R
@@ -4,46 +4,41 @@ library(tidyverse)
 library(rvest)
 library(tigris)
 library(sf)
+library(zoo)
 
 
-### get station names and drop some crappy stations
+### get station names from rnoaa
 station_names <- isd_stations()
-bad_stations <- c("72211800482", #Sarasota FL
-                  "72334703809", #Dyer, TN
-                  "99818099999", #Sanilac, MI
-                  "99726099999", #Sanilac MI
-                  "72522904858",
-                  "72340703953",
-                  "72028803711",
-                  "74003024103", #Tooele, UT
-                  "72575399999", #Tooele, UT
-                  "91197721508", #Also in the mountains on Hawaii
-                  "99999921514") #On top of a volcano at 11,000' in Hawaii
+### add moyr to filter out old stations that wont have data anymore
+station_names$end<- anytime::anydate(station_names$end)
+station_names$end_moyr <- zoo::as.yearmon(station_names$end)
 
+### do the filtering and create station_id
+### drop non US states
 station_names %>%
-  filter(station_name %nin% bad_stations,
-         ctry %in% "US") %>%
+  filter(ctry %in% "US",
+         end_moyr == "Nov 2021") %>%
   mutate(station_id = paste0(usaf, wban)) %>%
   filter(state %nin% c("", "PR", "VI")) -> station_names
 
 
-### pull map 
+### pull map using tigris - filter out non us states
 map <- tigris::counties(cb = T)
 map %>%
   filter(STATEFP %nin%c(72, 60, 66, 69, 78 )) -> map
 
-### get centroids of counties and create sf objecct
+### get centroids of counties and create sf object and create df from it
 centroids <- sf::st_centroid(map$geometry)
 centroids <- data.frame(t(sapply(centroids, function(x) unlist(x))))
 centroids <- sf::st_as_sf(centroids, coords=c("X1", "X2"))
 
 ### set crs for centroids since decimal degrees use 4269
 sf::st_crs(centroids) <- 4269
-### fix projection
+### fix projection to albers
 centroids<-tigris::shift_geometry(centroids)
 map<-tigris::shift_geometry(map)
 
-
+### remove station names that dont have lat/long and convert to sf
 station_names %>%
   filter(!is.na(lon),
          !is.na(lat)) -> station_names
@@ -51,45 +46,47 @@ station_names <- sf::st_as_sf(station_names, coords = c("lon", "lat"))
 
 ### set crs for centroids since decimal degrees use 4269
 sf::st_crs(station_names) <- 4269
-### fix projection
+### fix projection to albers
 station_names<-tigris::shift_geometry(station_names)
-
-
 
 ### find nearest weather station to centroid
 centroids %>% 
   #dplyr::group_by( id ) %>%
   dplyr::mutate( np = sf::st_nearest_feature( geometry, station_names )) -> test
-### this just gives centroid geometry and weather station name - need county name for centroid       
+### this  gives centroid geometry and weather station name - need county name for centroid       
 
-### add back useful stuff          
+### add back useful stuff from station_names       
 test$station_id <- station_names$station_id[test$np]                
 test$state <- station_names$station_name[test$np]                
 test$state <- station_names$state[test$np]                
+### manual spot check seems to work fine
 
-
-#st_geometry(test)<-NULL
-### put nearest station in map
+### put nearest station in map object
 map$station_id <- test$station_id
 
-### extract map
+### extract map and remove geometry
 stations <- map
 st_geometry(stations)<-NULL
 
 #write.table(stations, "/Users/timwiemken/Library/Mobile Documents/com~apple~CloudDocs/Work/Pfizer/covidtransmission_ecologic/stations_and_counties.csv", sep=",", row.names=F, na="")
 
+### several stations are matched to multiple centroids
+#sort(table(stations$station_id), decreasing=T)
 
-
-### uniques
+### uniques only
 unique.stations <- unique(stations$station_id)
 
-## clear
-rm(list=setdiff(ls(), c("unique.stations")))
+## clear environment
+rm(list=setdiff(ls(), c("unique.stations", "stations")))
 gc()
 
+
+### pull urls from ftp
 url2020 <- "https://www.ncei.noaa.gov/data/global-summary-of-the-day/access/2020/"
 url2021 <- "https://www.ncei.noaa.gov/data/global-summary-of-the-day/access/2021/"
 
+
+### scrape links using rvest
 url2020 %>% 
   read_html() %>%
   html_nodes("a") %>% 
@@ -97,10 +94,13 @@ url2020 %>%
 ### drop non csv files
 weather2020_links <- weather2020_links[grep(".csv", weather2020_links)]
 ## find just ids in our dataset
+### remove .csv from link then match to uniques only - things we want to pull
+### then add .csv back in for links
 weather2020_links <- gsub(".csv", "", weather2020_links)
 weather2020_links <- weather2020_links[weather2020_links%in%unique.stations]
 weather2020_links <- paste0(weather2020_links, ".csv")
 
+### same for 2021
 url2021 %>% 
   read_html() %>%
   html_nodes("a") %>% 
@@ -111,16 +111,20 @@ weather2021_links <- gsub(".csv", "", weather2021_links)
 weather2021_links <- weather2021_links[weather2021_links%in%unique.stations]
 weather2021_links <- paste0(weather2021_links, ".csv")
 
+### create full web link
 links2020 <- paste0(url2020, weather2020_links)
 links2021 <- paste0(url2021, weather2021_links)
 
+### map links to vroom to read in all data. 
 links2020%>%
   purrr::map(
     ~vroom::vroom(.)
   ) -> dat
 
-#test <- vroom::vroom(paste0(url2020, weather2020_links[1]))
-
+### bind to one df
 test <- data.table::rbindlist(dat)
 test$station_id <- as.character(test$STATION)
-test2 <- merge(test, stations, by="station_id", all.x=T)
+test <- as.data.frame(test)
+
+
+test2 <- merge(test, stations[unique(stations$station_id),], by="station_id", all=T)
